458b73bd066802eb484dd7676147090080985a80
Dean Glueckler <glueckler@gmail.com>
2020-02-07 13:48:12 -0800

    fixed capital letter issue in filename import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import ArticlePreview from "src/components/ArticlePreview"
+import ArticlePreview from "src/components/articlePreview"
 
 import "./index.scss"
 
